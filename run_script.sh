e0424ccc7c8252699b4353b36f3e7e7fe54d10c1
Michael M <michael.f.mantfeld@gmail.com>
2019-06-19 19:49:44 +1000

    Removed old files

--- run_script.sh
+++
@@ -1,6 +0,0 @@
-rm test.db
-sqlite3 test.db < sql/schema.sql
-python3 ticket_gen.py -n 1000 -o activities.json
-python3 data_insert.py -i activities.json -d test.db
-sqlite3 test.db < sql/update_data.sql
-sqlite3 test.db -header  < sql/ticket_summary.sql
